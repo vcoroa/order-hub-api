699ba01f3c0c19555c427f9d231d9056d441ee8e
Vanilton Coroa <vanilton.coelho@gmail.com>
2025-06-14 16:31:41 -0300

    first commit

---
+++ src/main/java/br/com/vpsconsulting/order_hub_api/OrderHubApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.vpsconsulting.order_hub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderHubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vpsconsulting/order_hub_api/OrderHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vpsconsulting.order_hub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
